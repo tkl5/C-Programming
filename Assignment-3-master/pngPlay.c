d48ad7b684882ef595db9e844a8032fa37c69594
Tim Liu <tkl5@Uw.edu>
2016-08-03 21:16:15 -0700

    Update pngPlay.c

--- Assignment-3-master/pngPlay.c
+++ Assignment-3-master/pngPlay.c
@@ -12,6 +12,24 @@ int main(int argc, char *argv[]){
   //-r is followd by the rotation angle in degrees (float) <optional for user>
   //-g is whether th png should be grayed <optional for user>
   
+  int i = 1;
+
+  while (i < argc) {
+	  if (strcmp(argv[i], "-i") == 0) {
+		  inputfile = argv[++i];
+
+	  } else if (strcmp(argv[i], " -o") == 0) {
+		  outputfile = argv[++i];
+
+	  } else if (strcmp(argv[i], "-g") == 0) {
+		  grayFlag = 1.0;
+
+	  } else if (strcmp(argv[i], "-r") == 0) {
+		  degrees = atof(argv[++i]);
+	  }
+	  i++;
+  }
+  
   pixMap *p=pixMap_init_filename(inputfile);
   if(degrees)pixMap_rotate(p,degrees);
   if(grayFlag)pixMap_gray(p);
